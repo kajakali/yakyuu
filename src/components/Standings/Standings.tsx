e865dbc089d8960874327bd06d5a043d6ea524df
Frieda Jacobson <kajakali@hotmail.com>
2020-07-27 12:10:57 -0500

    ice cream added to display

--- src/components/Standings/Standings.tsx
+++ src/components/Standings/Standings.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 
 
-function Standings() {
-  return (
-    <div className="Standigns">
-        <h1>Scores</h1>
-        <p>
-          <a href="mlb.com/standings">Standings</a>
-        </p>
-    </div>
-  );
+class Standings extends React.Component {
+    state = ({
+        iceCream: "mint"
+    })
+    multiply = () => {
+        if(Math.random() > 0.5){
+            this.state.iceCream = "mint and cinnamon"
+        }
+    return <p>{this.state.iceCream}</p>
+    }
+    render(){
+        return (
+            <div className="Standigns">
+                <h1>Scores</h1>
+                <p>
+                  <a href="mlb.com/standings">Standings</a>
+                  {this.multiply()}
+ 
+                </p>
+            </div>
+          );
+    }
+
 }
 
 export default Standings;
