6b0c7056ff22a46bbf02278c4355cdcced402848
Frieda Jacobson <kajakali@hotmail.com>
2020-07-27 12:02:17 -0500

    scores and standings links

---
+++ src/components/Scores/Scores.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Scores() {
+  return (
+    <div className="Scores">
+        <h1>Scores</h1>
+        <p>
+          <a href="mlb.com/scores">Scores</a>
+        </p>
+    </div>
+  );
+}
+
+export default Scores;
---
+++ src/components/Standings/Standings.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Standings() {
+  return (
+    <div className="Standigns">
+        <h1>Scores</h1>
+        <p>
+          <a href="mlb.com/standings">Standings</a>
+        </p>
+    </div>
+  );
+}
+
+export default Standings;
