c156db33011bda03cb38b92a22f9b5bf087c77f9
Frieda Jacobson <kajakali@hotmail.com>
2020-08-03 14:20:53 -0500

    the second hand is better sized

--- src/components/Clock/Second.jsx
+++ src/components/Clock/Second.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Second() {
   return (
     <g id="second">
-        <path class="second-arm" d="M300.5 350V55"/>
+        <path class="second-arm" d="M300.5 330V55"/>
         <circle class="sizing-box" cx="300" cy="300" r="253.9"/>
     </g>
   );
